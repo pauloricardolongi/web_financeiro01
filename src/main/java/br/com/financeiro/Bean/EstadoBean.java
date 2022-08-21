9b823824cc4b3d2cba2f43265b9e0dde1af12657
pauloricardolongi <pauloricardolongi@gmail.com>
2022-08-21 06:22:14 -0300

    Salvando e Excluindo Estados

--- src/main/java/br/com/financeiro/Bean/EstadoBean.java
+++ src/main/java/br/com/financeiro/Bean/EstadoBean.java
@@ -1,34 +1,32 @@
 package br.com.financeiro.Bean;
 
+
+
 import java.io.Serializable;
-
-
 
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.persistence.PostUpdate;
+import javax.faces.event.ActionEvent;
 
-import org.jboss.logging.annotations.Pos;
 import org.omnifaces.util.Messages;
 
 import br.com.financeiro.dao.EstadoDAO;
 import br.com.financeiro.domain.Estado;
-
-
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class EstadoBean implements Serializable {
 	private Estado estado;
-	private List<Estado>estados;
-	
+	private List<Estado> estados;
+
 	public List<Estado> getEstados() {
 		return estados;
 	}
+
 	public void setEstados(List<Estado> estados) {
 		this.estados = estados;
 	}
@@ -45,26 +43,24 @@ public class EstadoBean implements Serializable {
 		estado = new Estado();
 	}
 
-
 	public void salvar() {
 		try {
 			EstadoDAO estadoDAO = new EstadoDAO();
 			estadoDAO.salvar(estado);
 
 			novo();
-
+			estados = estadoDAO.listar();
+			
 			Messages.addGlobalInfo("Estado salvo com sucesso");
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado");
 			erro.printStackTrace();
 		}
 	}
-	
 
-
-@PostConstruct
-	public void listar(){
-		try{
+	@PostConstruct
+	public void listar() {
+		try {
 			EstadoDAO estadoDAO = new EstadoDAO();
 			estados = estadoDAO.listar();
 		} catch (RuntimeException erro) {
@@ -72,8 +68,21 @@ public class EstadoBean implements Serializable {
 			erro.printStackTrace();
 		}
 	}
+	
 
+	public void excluir(ActionEvent evento) {
+		try {
+			estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.excluir(estado);
+			
+			estados = estadoDAO.listar();
+
+			Messages.addGlobalInfo("Estado removido com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o estado");
+			erro.printStackTrace();
+		}
+	}
 }
-
-
-
